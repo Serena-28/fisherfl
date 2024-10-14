0c9e1e7b0383b0bde76a3e3170fe1b9310c9a9be
Hjt-2002 <70947336+Hjt-2002@users.noreply.github.com>
2024-10-14 13:42:08 +0800

    Update message_define.py

--- api/distributed/fedinitprune/message_define.py
+++ api/distributed/fedinitprune/message_define.py
@@ -8,7 +8,6 @@ class MyMessage(object):
 
     # client to server
     MSG_TYPE_C2S_SEND_MODEL_TO_SERVER = 3
-    MSG_TYPE_C2S_SEND_STATS_TO_SERVER = 4
 
     MSG_ARG_KEY_TYPE = "msg_type"
     MSG_ARG_KEY_SENDER = "sender"
